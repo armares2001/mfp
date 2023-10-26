22395ac21b51d71e7c25ab15ccce9a28ae696b1d
armares2001 <armares2001@libero.it>
2023-10-26 23:12:27 +0200

    updated container and deploy action for marketing

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new Mfp({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: {
         ...deps,
