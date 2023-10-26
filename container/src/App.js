a3951b7b59e57f633cfb4c31bec5b11a03a4df22
armares2001 <armares2001@libero.it>
2023-10-26 21:36:41 +0200

    test

--- container/src/App.js
+++ container/src/App.js
@@ -1,17 +1,16 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import MarketingApp from "./components/MarketingApp";
 
 // import { mount as marketingMount } from "marketing/App";
 // import App from "marketing/jsx";
 
-export default ()=>{
-    return (
-        <Fragment>
-            <h1>Hi there!!</h1>
-            <hr/>
-            <MarketingApp/>
-            {/*<App/>*/}
-        </Fragment>
-    );
-
+export default () => {
+  return (
+    <Fragment>
+      <h1>Hi there!!</h1>
+      <hr />
+      <MarketingApp />
+      {/*<App/>*/}
+    </Fragment>
+  );
 };
