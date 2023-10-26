a91078305ad59c5943a8d358621ecf706204c5e8
armares2001 <armares2001@libero.it>
2023-10-26 22:05:55 +0200

    added publicpath

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -1,37 +1,38 @@
-const {merge}=require("webpack-merge");
-const commonConfig=require("./webpack.common");
-const Mfp=require("webpack/lib/container/ModuleFederationPlugin");
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common");
+const Mfp = require("webpack/lib/container/ModuleFederationPlugin");
 const deps = require("./../package.json").dependencies;
 
-const domain=process.env.PRODUCTION_DOMAIN;
+const domain = process.env.PRODUCTION_DOMAIN;
 
-const prodConfig={
-    mode:"production",
-    output:{
-        filename:"[name].[contenthash].js"
-    },
-    plugins:[
-        new Mfp({
-            name:"container",
-            remotes:{
-                marketing:`marketing@${domain}/marketing/remoteEntry.js`
-            },
-            shared: {
-                ...deps,
-                react: {
-                    singleton: true,
-                    requiredVersion: deps.react,
-                },
-                "react-dom": {
-                    singleton: true,
-                    requiredVersion: deps["react-dom"],
-                },
-            },
-        }),
-    ]
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+    publicPath: "/container/latest/",
+  },
+  plugins: [
+    new Mfp({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+  ],
 };
 
-const config=merge(commonConfig,prodConfig);
-console.log(config)
+const config = merge(commonConfig, prodConfig);
+console.log(config);
 
-module.exports=config;
+module.exports = config;
