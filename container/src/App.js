ec4d354488f764514c4372156bbcb9884314bece
armares2001 <armares2001@libero.it>
2023-10-28 19:35:44 +0200

    fixed containerApp

--- container/src/App.js
+++ container/src/App.js
@@ -1,23 +1,21 @@
 import React, { Fragment } from "react";
-// import MarketingApp from "./components/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom";
-import { Link as RouterLink } from "react-router-dom";
-// import { BrowserRouter } from "react-router-dom/cjs/react-router-dom";
+
 // import { mount as marketingMount } from "marketing/App";
 // import App from "marketing/jsx";
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
+      <Fragment>
         <Header />
-        <RouterLink to="/">ciAo</RouterLink>
         <p>Hi!</p>
         <hr />
-        {/* <MarketingApp /> */}
+        <MarketingApp />
         {/*<App/>*/}
-      </div>
+      </Fragment>
     </BrowserRouter>
   );
 };
