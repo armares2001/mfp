005415474fcc954bd1a68874fc071badf262f184
armares2001 <armares2001@libero.it>
2023-10-28 16:43:38 +0200

    test

--- container/src/App.js
+++ container/src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 import MarketingApp from "./components/MarketingApp";
-import Header from "./components/Header";
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom";
+// import Header from "./components/Header";
+// import { BrowserRouter } from "react-router-dom/cjs/react-router-dom";
 // import { mount as marketingMount } from "marketing/App";
 // import App from "marketing/jsx";
 
 export default () => {
   return (
-    <BrowserRouter>
-      <Header />
+    <Fragment>
+      {/* <Header /> */}
+      <p>Hi!</p>
+      <hr />
       <MarketingApp />
       {/*<App/>*/}
-    </BrowserRouter>
+    </Fragment>
   );
 };
