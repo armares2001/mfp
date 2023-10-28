30f7f4f3bae96aded9a843bbeaf04adcc0330096
armares2001 <armares2001@libero.it>
2023-10-28 15:59:48 +0200

    added Header

--- container/src/App.js
+++ container/src/App.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from "react";
 import MarketingApp from "./components/MarketingApp";
-
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom";
 // import { mount as marketingMount } from "marketing/App";
 // import App from "marketing/jsx";
 
 export default () => {
   return (
-    <Fragment>
-      <h1>Hi there!!!!!!</h1>
-      <hr />
+    <BrowserRouter>
+      <Header />
       <MarketingApp />
       {/*<App/>*/}
-    </Fragment>
+    </BrowserRouter>
   );
 };
